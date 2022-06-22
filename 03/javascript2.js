fc4fb3b221500e72a09995b6fdea54998af8b330
chloe0330 <90747058+chloe0330@users.noreply.github.com>
2022-06-22 17:51:08 +0900

    Add files via upload

---
+++ 03/javascript2.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var head = document.getElementById('head');
+
+    head.addEventListener('click', function(){
+        var date = new Date();
+        alert(date);
+    });
+}
